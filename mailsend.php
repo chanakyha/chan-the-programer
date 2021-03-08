638be938941011addc5e6ac9297e741469a70824
Chan <66877639+chanakyha@users.noreply.github.com>
2021-03-08 16:45:36 +0530

    Update mailsend.php

--- mailsend.php
+++ mailsend.php
@@ -6,27 +6,7 @@
 
     <?php
 
-        require_once('PHPMailer-5.2-stable/PHPMailerAutoload.php');
-
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->SMTPAuth = true;
-        $mail->SMTPSecure = 'ssl';
-        $mail->Host = 'smtp.gmail.com';
-        $mail->Port ='465';
-        $mail->isHTML();
-        $mail->Username = 'vetrichanakyha2003@gmail.com';
-        $mail->Password = 'dkbvcfxhmfawscnu';
-        $mail->SetFrom('vetrichanakyha2003@gmail.com');
-        $mail->Subject = $_POST["Subject"] . ' from ( ' . $_POST["name"] . ' )';
-        $mail->Body = 'From:' . $_POST['name'] .'<br> His Mail Address: '. $_POST['_replyto'] . '<br>' . $_POST['message'];
-        $mail->AddAddress('vetrichanakyha2003@gmail.com');
-
-        $mail->Send();
-
-        echo '<center><h1>Request SENT SUCCESSFULLY</h1><hr><p>Redirecting to Main Page Please Wait.....</p></center>';
-
-        header("Location: index.html");
+        header("Location: http://chan-the-programer.freetzi.com/");
   
         exit; 
 
